c0fa3d23fa2b4dfc5a719d7cf568f81bc2a64ff2
mombanger69 <hugiholm1@gmail.com>
2019-02-02 12:43:56 +0000

    uppsetning

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const bodyParser = require("body-parser");
+
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
