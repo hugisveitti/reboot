e367b56328061e33a783d4287fe4d4b86c399ed0
ingisteinn <ingi.gudmunds@gmail.com>
2019-02-03 03:13:37 +0000

    bot

--- app/post.js
+++ app/post.js
@@ -1,24 +1,44 @@
+require('dotenv').config();
+
 var Events = require('./models/events.js');
 var path = require("path");
+var request = require('request');
+var querystring = require('querystring');
 
-module.exports = function(app){
 
 
+module.exports = function (app) {
 
-    app.post("/addevent", (req, res) => {
-        console.log(req.body)
-        var event = new Events(req.body);
+  app.post("/addevent", (req, res) => {
+    console.log(req.body)
+    var event = new Events(req.body);
 
-        event.save((err, event) => {
-            if(err){
-                console.log("error", err);
-                res.status(500).send("database error");
-            }
-            else {
-                console.log("success")
-                res.redirect("/addEvent");
-            }
-        });
-
+    event.save((err, event) => {
+      if (err) {
+        console.log("error", err);
+        res.status(500).send("database error");
+      } else {
+        console.log("success")
+        res.redirect("/addEvent");
+      }
     });
+
+  });
+
+  app.post("/askbot", (req, res) => {
+    var message = req.body.message;
+    
+    var endpointKey = process.env.LUIS_MODEL_URL;
+    // append query string to endpoint URL
+    var luisRequest = endpointKey + message;
+    // HTTP Request
+    request(luisRequest,
+      function (err, response, body) {
+        if (err)
+          console.log(err);
+        else {
+          res.send(JSON.parse(body));
+        }
+      });
+  })
 }
